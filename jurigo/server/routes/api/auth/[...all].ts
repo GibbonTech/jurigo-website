cdc16d0e06921eb6538c8886013f674bf7ad6b29
Rick <yasboss2412@gmail.com>
2025-12-28 01:12:41 +0000

    Restore original auth route and remove broken Nitro server route

--- jurigo/server/routes/api/auth/[...all].ts
+++
@@ -1,7 +0,0 @@
-import { defineEventHandler, toWebRequest } from 'h3'
-import { auth } from '../../../../src/lib/auth'
-
-export default defineEventHandler(async (event) => {
-  const request = toWebRequest(event)
-  return auth.handler(request)
-})
